cc2352df1c5e4344f5b4cc9790b49fdd83238125
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2017-07-31 21:19:08 +0900

    Fix test_model script

--- scripts/test_model.sh
+++ scripts/test_model.sh
@@ -12,41 +12,67 @@ exec_test() {
             $(dirname $0)/gen_program.sh ${model} ${example_dir}/$1-example ${max_length} $2" \
             2>&1 > /dev/null
         result=$?
+        echo ${result}
         if [ $result -eq 124 ]
         then
             echo ${timeout_time}
         elif [ $result -ne 0 ]
         then
-            echo "timeout"
+            echo "error"
         fi
     ) | tail -1
 }
 
 do_test() {
     id=$1
-    index=$1
     while true
     do
-        if [ -e "${example_dir}/${index}-example" ]
-        then
-            echo "Id(${id})-${index}: Start"
-            time_dfs=$(exec_test $index dfs)
-            echo "Id(${id})-${index}: DFS ${time_dfs}"
-            time_none=$(exec_test $index none)
-            echo "Id(${id})-${index}: None ${time_none}"
-            echo ${index} ${time_none} ${time_dfs} >> $(dirname $0)/result-${id}
-            echo "Id(${id})-${index}: Finish"
-        else
-            break
-        fi
-        index=$((${index} + ${parallel_num}))
+        x=$(cat /tmp/job-queue)
+        for i in ${x}
+        do
+            echo ${i} "#i"
+            if [ $i = "END" ]
+            then
+                return 0
+            fi
+            if [ -e "${example_dir}/${i}-example" ]
+            then
+                echo "Id(${id})-${i}: Start"
+                time_dfs=$(exec_test $i dfs)
+                echo "Id(${id})-${i}: DFS ${time_dfs}"
+                time_sae=$(exec_test $i)
+                echo "Id(${id})-${i}: SAE ${time_sae}"
+                time_none=$(exec_test $i none)
+                echo "Id(${id})-${i}: None ${time_none}"
+                echo ${i} ${time_none} ${time_sae} ${time_dfs} >> $(dirname $0)/result-${id}
+                echo "Id(${id})-${i}: Finish"
+            fi
+        done
     done
 }
 
-for i in $(seq 1 ${parallel_num})
+enqueue_jobs() {
+    for i in $(seq 0 $(( $(ls ${example_dir}/*-example | wc -l) - 1)))
+    do
+        echo $i > /tmp/job-queue
+    done
+    for i in $(seq 1 ${parallel_num})
+    do
+        echo END > /tmp/job-queue
+    done
+}
+
+
+
+rm /tmp/job-queue
+mkfifo /tmp/job-queue
+
+for i in $(seq ${parallel_num})
 do
-    do_test $i &
+    do_test ${i} &
 done
+
+enqueue_jobs
 
 wait
 
